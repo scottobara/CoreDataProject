5764d0fec6cc382e2f7eecd238529d4c92cfe677
Scott Obara <scott@rubbscreations.com>
2021-02-10 21:10:03 +1100

    Customising a Core Data class

---
+++ CoreDataProject/Movie+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Movie+CoreDataClass.swift
+//  CoreDataProject
+//
+//  Created by Scott Obara on 10/2/21.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Movie)
+public class Movie: NSManagedObject {
+
+}
